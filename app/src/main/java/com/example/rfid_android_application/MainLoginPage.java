1a6cef6d3e4f3f135137622f05d330f94f65440c
AyushVerekar <ayushverekar49@gmail.com>
2025-01-18 21:42:42 +0530

    Updated basic colour palatte such as background and button colour. Also added links to button such that the views are accessible on button actions. validation of data and other major changes will be added in subsequent updates.

--- app/src/main/java/com/example/rfid_android_application/MainLoginPage.java
+++ app/src/main/java/com/example/rfid_android_application/MainLoginPage.java
@@ -1,6 +1,9 @@
 package com.example.rfid_android_application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,29 @@ public class MainLoginPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_login_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        Button studentLoginButton;
+        Button teacherLoginButton;
+
+        studentLoginButton=findViewById(R.id.MainLoginPageStudentButton);
+        teacherLoginButton=findViewById(R.id.MainLoginPageTeacherButton);
+
+        studentLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainLoginPage.this,LoginStudentPage.class);
+                startActivity(intent);
+            }
+        });
+
+        teacherLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainLoginPage.this,LoginTeacherPage.class);
+                startActivity(intent);
+            }
         });
     }
 }
