c05e7fec6f0f3f324f55515aada5069a095db561
Sakshivijapure <vijapuresakshi123@gmail.com>
2025-02-03 14:44:52 +0530

    craeted timetable with 8 columns and 7 rows for teachers and students.fetched data from database.

--- app/src/main/java/com/example/rfid_android_application/Home_Page.java
+++ app/src/main/java/com/example/rfid_android_application/Home_Page.java
@@ -1,6 +1,9 @@
 package com.example.rfid_android_application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,16 @@ public class Home_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button csStudentsTimetable;
+        csStudentsTimetable = findViewById(R.id.HomeTimeTableButton);
+
+        csStudentsTimetable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home_Page.this, CsStudentTimetable  .class);
+                startActivity(intent);
+            }
         });
     }
 }
