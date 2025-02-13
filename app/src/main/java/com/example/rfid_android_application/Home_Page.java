76523a8d76fef51dc32a0b2810a94fb7b33a89a4
Angeeth <angeethpavithran664@gmail.com>
2025-02-13 14:33:43 +0530

    updated LoginStudentPage.java and LoginTeacherPage.java  to verify data from the database and added shared preferences to store logged in users data

--- app/src/main/java/com/example/rfid_android_application/Home_Page.java
+++ app/src/main/java/com/example/rfid_android_application/Home_Page.java
@@ -1,30 +1,44 @@
 package com.example.rfid_android_application;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home_Page extends AppCompatActivity {
 
+    Button logoutButton;
+    SharedPreferences studentPrefs, teacherPrefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
 
-        Button csStudentsTimetable;
-        csStudentsTimetable = findViewById(R.id.HomeTimeTableButton);
+        logoutButton = findViewById(R.id.logoutButton);
 
-        csStudentsTimetable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Home_Page.this, StudentTimetable.class);
-                startActivity(intent);
-            }
-        });
+        studentPrefs = getSharedPreferences("StudentPrefs", Context.MODE_PRIVATE);
+        teacherPrefs = getSharedPreferences("TeacherPrefs", Context.MODE_PRIVATE);
+
+        logoutButton.setOnClickListener(v -> logoutUser());
+    }
+
+    private void logoutUser() {
+
+        SharedPreferences.Editor studentEditor = studentPrefs.edit();
+        studentEditor.clear();
+        studentEditor.apply();
+
+        SharedPreferences.Editor teacherEditor = teacherPrefs.edit();
+        teacherEditor.clear();
+        teacherEditor.apply();
+
+        Intent intent = new Intent(Home_Page.this, MainLoginPage.class);
+        startActivity(intent);
+        finish();
     }
 }
